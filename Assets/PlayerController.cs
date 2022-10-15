8346c47eb5851b1041461158dcc38751ac4df910
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-10-15 10:43:11 +0900

    feat: Walking/Idle animation with needed fixes feat: Better acceleration/deceleration and jump feat: Real collision check for canJump()

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,8 +12,11 @@ public class PlayerController : MonoBehaviour
     private float deceleration = 0.0f;
     private Rigidbody2D _rigidBody;
 
+    private bool _goingLeft = false;
     
     private bool _canMove;
+
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
@@ -22,25 +25,48 @@ public class PlayerController : MonoBehaviour
         if (_rigidBody == null)
             Debug.LogError("please add a Rigidbody2D component");    
         deceleration = acceleration * 5;
+        _animator = GetComponent<Animator>();
     }
 
-    bool canJump() => true;
+    bool canJump() {
+        const int playerMask = (1 << 3);
+        int everythingExpectPlayerMask = ~playerMask;
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.7f, everythingExpectPlayerMask);
+    }
 
+    void FlipSprite() {
+        var tempScale = transform.localScale;
+        tempScale.x *= -1;
+        transform.localScale = tempScale;
+        _goingLeft = !_goingLeft;
+    }
 
     void Move()
     {
         float input = Input.GetAxisRaw("Horizontal");
+        bool oppositeDirection = _goingLeft && input < 0 || !_goingLeft && input > 0;
+        bool oppositeVelocity = _goingLeft && _rigidBody.velocity.x < 0 || !_goingLeft && _rigidBody.velocity.x > 0;
+        
+        if (oppositeDirection) {
+            FlipSprite();
+        }
 
-        if ((input > 0 && playerSpeed < maxPlayerSpeed) || (input < 0 && playerSpeed > -maxPlayerSpeed)  )
-            playerSpeed += (acceleration * input) * Time.deltaTime;
-        else {
+        if ((input > 0 && playerSpeed < maxPlayerSpeed) || (input < 0 && playerSpeed > -maxPlayerSpeed)) {
+            playerSpeed += (oppositeVelocity ? deceleration : acceleration) * input * Time.deltaTime;
+        } else {
             if(playerSpeed > deceleration * Time.deltaTime)
                 playerSpeed = playerSpeed - deceleration * Time.deltaTime;
             else if(playerSpeed < -deceleration * Time.deltaTime)
                 playerSpeed = playerSpeed + deceleration * Time.deltaTime;
-            else
+            else {
                 playerSpeed = 0;
+            }
         }
+        if (input == 0) {
+            _animator.Play("Idle");
+        } else 
+            _animator.Play("Walking");
+        
         _rigidBody.velocity = new Vector2(playerSpeed * Time.deltaTime, _rigidBody.velocity.y);
     }
     private void Jump() => _rigidBody.velocity = new Vector2(0, jumpPower);
