74142b69a5f6cf278b8e6fb943fd8973aeef18fe
tomasit <thomas.itel@epitech.eu>
2022-12-16 11:26:47 +0900

    [ADD] respawn

---
+++ Assets/Scripts/UI/DeathMenu/RespawnManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class RespawnManager : MonoBehaviour
+{
+    private Transform _player;
+    private TransitionScreen _screenTransition;
+    private bool _isDead = false;
+
+    private void Start()
+    {
+        _screenTransition = FindObjectOfType<TransitionScreen>();
+        _player = FindObjectOfType<PlayerController>().transform;
+    }
+
+    private IEnumerator DeathCoroutine()
+    {
+        _screenTransition.StartBeginTransition("Death");
+        while (_screenTransition.GetTransitionState() != TransitionScreen.TransitionState.END)
+            yield return null;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Update()
+    {
+        if (!_isDead && _player.GetComponent<Health>().isDead())
+        {
+            _isDead = true;
+            StartCoroutine(DeathCoroutine());
+        }
+    }
+}
+    
